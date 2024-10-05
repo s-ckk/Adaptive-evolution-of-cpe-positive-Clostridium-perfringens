07f5df6f094e24c5979038efbdd687bac7e8cfc2
s-ckk <1351332788@qq.com>
2024-10-05 19:10:33 +0800

    Delete file

--- figure1/figure1C.R
+++
@@ -1,5 +0,0 @@
-library(ggplot2)
-data <- readxl::read_xlsx("figure1C.xlsx")
-ggplot(data, aes(x = source, y = number, fill=location))+
-  geom_bar(stat = "identity", position="stack")+
-  theme_bw()
